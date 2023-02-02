17d6c200fc11636d0c9797d3d4da4c45a1cb5075
bkoj-arch <loupfourment@gmail.com>
2023-02-02 15:08:22 +0100

    maj

--- main.py
+++ main.py
@@ -3,4 +3,5 @@
 print("Content-type: text/html")
 print("")
 
-print("HELLO TOUT LE MONDE")
+print("HELLO TOUT LE MONDE ca fonctionne! ")
+print("A voir pour la cle privee jenkins, et permissions proftpd")
